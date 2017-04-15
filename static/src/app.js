763fae1e22c40604af88ad730b2bb63f5790d77a
Felix Kaiser <felix.kaiser@fxkr.net>
2017-04-15 12:38:52 +0200

    frontend: Add basic form to use the print method

--- static/src/app.js
+++ static/src/app.js
@@ -1,6 +1,20 @@
 import Bootstrap from 'bootstrap/dist/css/bootstrap.css';
 require('./app.styl')
 
+var target = "http://localhost:8081";
+
+function onPrintButtonClick(e) {
+  e.preventDefault();
+
+  $.post({
+    url: target + "/printer/v1/print",
+    data: JSON.stringify({text: $("#label-text").val()}),
+    dataType: "json",
+  }).done(function(data) {
+    console.log("done", data);
+  });
+}
+
 $().ready(() => {
-  console.log("ready");
+  $("#print-button").click(onPrintButtonClick);
 });
