c2273672ae095a588d083272f9e030c065d01390
Felix Kaiser <felix.kaiser@fxkr.net>
2017-04-16 13:58:39 +0200

    frontend: Show status notifications

--- static/src/app.js
+++ static/src/app.js
@@ -2,18 +2,32 @@ import 'file-loader?name=[name].[ext]!./index.html';
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'github-fork-ribbon-css/gh-fork-ribbon.css';
+import 'noty/lib/noty.css';
 import './app.styl';
 
+import Noty from 'noty';
 
 function onPrintButtonClick(e) {
   e.preventDefault();
+
+  var note = new Noty({
+    text: 'Printing...',
+    type: 'info',
+    layout: 'bottomRight',
+  }).show();
 
   $.post({
     url: "/api/v1/printer/print",
     data: JSON.stringify({text: $("#label-text").val()}),
     dataType: "json",
-  }).done(function(data) {
-    console.log("done", data);
+  }).done((data) => {
+    note.setText("Label printed successfully.");
+    note.setType('success');
+    note.setTimeout(1000);
+  }).fail((data) => {
+    note.setText("Failed to print label.");
+    note.setType('error');
+    note.setTimeout(3000);
   });
 }
 
